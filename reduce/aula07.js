e87c463613806621d46b5f8a3f719214cef29300
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-07-09 16:18:00 -0300

    update reduces

---
+++ reduce/aula07.js
@@ -0,0 +1,56 @@
+/** Filtro com validação e Formatação 2 */
+const data = require('./base2')
+
+let filtroMes = "janeiro"
+//let filtroMes = "fevereiro"
+
+let real = Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+});
+
+function filtro1(){
+    try{
+        let filtro = data.contas
+        .filter( item => item.mes === filtroMes)
+        .map( chave => chave.valor)
+
+        let total = filtro.reduce((acc, total) => acc + total)
+
+        return console.log(
+            "Soma das contas mês: " + 
+            filtroMes + ": " + real.format(total)
+        )
+    }catch(error){
+        console.log("Erro na consulta")
+    }
+}
+
+function filtro2(){
+    try{
+        let filtro = data.contas
+        let nome = filtro[1].descricao
+        let valor = filtro[1].valor
+
+        console.log("Nome: " + nome, " - " + "Valor: " + valor)
+    }catch(error){
+        console.log("Erro na consulta")
+    }
+}
+
+function filtro3(){
+    try{
+        let filtro = data.contas
+        console.log(filtro)
+    }catch(error){
+        console.log("Erro na consulta")
+    }
+}
+
+function start(){
+    filtro1()
+    filtro2()
+    filtro3()
+}
+
+start()
