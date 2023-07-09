e52a838f4e48c19096e739dff5226de370f05d60
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-07-09 15:54:40 -0300

    update lista com soma

---
+++ reduce/base2.js
@@ -0,0 +1,17 @@
+module.exports = {
+    contas: [
+        {"id": 1, "descricao": "Paulo",     "valor": 1700,  "mes": "janeiro", status: "Aguardando"},
+        {"id": 2, "descricao": "Diomar",    "valor": 3900,  "mes": "janeiro", status: "Aguardando"},
+        {"id": 3, "descricao": "Dora",      "valor": 3000,  "mes": "janeiro", status: "Aguardando"},
+        {"id": 4, "descricao": "Pai",       "valor":  400,  "mes": "janeiro", status: "Aguardando"},
+        {"id": 5, "descricao": "Consorcio", "valor": 400,   "mes": "janeiro", status: "Aguardando"},
+        {"id": 6, "descricao": "Diogo",     "valor": 700,   "mes": "janeiro", status: "Aguardando"},
+        {"id": 7, "descricao": "Mayke",     "valor": 1200,  "mes": "janeiro", status: "Aguardando"},
+        {"id": 8, "descricao": "Kenned",    "valor": 5000,  "mes": "janeiro", status: "Aguardando"},
+
+        {"id": 9, "descricao": "Cleber",    "valor": 3000,  "mes": "janeiro", status: "Pendente"},
+
+
+    ],
+    
+}
